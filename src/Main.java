3c3fa11715a6400e9c0e2c89ac12cdcffd1434d5
Marcus Cauê <marcuscaue.dev@gmail.com>
2023-09-15 16:15:23 -0300

    Adicionando cores

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import classes.*;
 import java.util.ArrayList;
 
 public class Main {
+
+  public static final String RESET = "\u001B[0m"; 
+  public static final String AMARELO = "\u001B[33m"; 
+  public static final String VERDE = "\u001B[32m";
+  public static final String CIANO = "\u001B[36m";
+
   public static void main(String[] args) {
 
     ArrayList<Player> players = new ArrayList<Player>();
@@ -18,19 +24,40 @@ public class Main {
 
     ArrayList<SimilarityPlayers> comparacoes = panlz.compararJogadores(players.get(0));
     
-    System.out.println("Jogador Escolhido | Jogador Comparado | Passes JE | Passes JC | Assistências JE |  Assistências JC | Similaridade |");
+    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
     for (SimilarityPlayers comp : comparacoes) {
-      Player p1 = comp.getP1();
+      Player p1 = comp.getP1(); 
+      char firstLetterP1 = p1.getName().charAt(0);
+      
       Player p2 = comp.getP2();
+      char firstLetterP2 = p2.getName().charAt(0);
+
       double similarity = comp.getSimilarity();
 
-      System.out.printf(
-        "\n%s | %s |\t %d%% | %d%% | %d%% | %d%% | %.0f%% |\n",
-        p1.getName(), p2.getName(), p1.getPercPasses(),
-        p2.getPercPasses(), p1.getPercAssists(), p2.getPercAssists(),
-        similarity
+      System.out.printf("%s%s%s (%s'%c'%s) em relação à %s%s%s (%s'%c'%s) possuem %s%.0f%%%s de similaridade: \n\n",
+        AMARELO, p1.getName(), RESET,
+        AMARELO, firstLetterP1, RESET,
+        VERDE, p2.getName(), RESET,
+        VERDE, firstLetterP2, RESET,
+        CIANO, similarity, RESET
       );
+
+      System.out.printf("Passes:%s      %d%%%s(%s'%c'%s) | %s%d%%%s (%s'%c'%s)\n", 
+        AMARELO, p1.getPercPasses(), RESET,
+        AMARELO, firstLetterP1, RESET, 
+        VERDE, p2.getPercPasses(), VERDE,
+        VERDE, firstLetterP2, RESET
+      );
+
+      System.out.printf("Assitências:%s %d%%%s(%s'%c'%s) | %s%d%%%s (%s'%c'%s)\n",
+        AMARELO, p1.getPercAssists(), RESET,
+        AMARELO, firstLetterP1, RESET, 
+        VERDE, p2.getPercAssists(), RESET,
+        VERDE, firstLetterP2, RESET
+      );
+
+      System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
     }
 
   }
