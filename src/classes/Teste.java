3391523a4753a48281a1b5ce00559583e706d516
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-09-25 14:18:26 -0300

    Similaridade de cossenos

--- src/classes/Teste.java
+++ src/classes/Teste.java
@@ -1,5 +1,23 @@
 package classes;
 
 public class Teste {
-  
+
+    public static double simiCos(int[] attrOne, int[] attrTwo) {
+        int sumOneTwo = 0;
+        double compriOne = 0, compriTwo = 0;
+
+        for (int i = 0; i < attrOne.length; i++) {
+            sumOneTwo += attrOne[i] * attrTwo[i];
+            compriOne += attrOne[i] * attrOne[i];
+            compriTwo += attrTwo[i] * attrTwo[i];
+        }
+
+        compriOne = Math.sqrt(compriOne);
+        compriTwo = Math.sqrt(compriTwo);
+
+        double similarity = sumOneTwo / (compriOne * compriTwo);
+
+        return similarity;
+    }
+
 }
